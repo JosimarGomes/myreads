58076993e23cf2e29ed8fa833bd34e8192f72d61
JosimarGomes <josimar.gomes@superlogica.com>
2017-10-10 19:02:42 -0300

    Update Search.js

--- src/components/Search.js
+++ src/components/Search.js
@@ -58,26 +58,28 @@ export default class Search extends Component {
 
         BooksAPI.search(wordSugestion).then(
             allBooks => {
-                console.log('allbo', allBooks)
                 if(allBooks.error) {
-                    console.log('deu errooo->>')
                     this._renderSugestions();
                 }
                 else {
                     _.remove(allBooks, (book, index)=>{
                         return index > 6;
                     })
+                    _.map(allBooks, (book)=>{
+                        _.map(mybooks, (mybook)=>{
+                            if(book.id === mybook.id)
+                                book.shelf = mybook.shelf;
+                        })
+                    })
+                    
                     this.setState({allBooks, title:'Suggestions for you...'});
                     this.props.renderLoading(false);
                 };
 
             }
         );
-
-        console.log('allBooks->>', allBooks)
-        console.log('myboo', wordSugestion)
+        
         return;
-
     }
     _reloadshelf() {
         const inputQuery = document.getElementById("query");
@@ -99,16 +101,7 @@ export default class Search extends Component {
                 <div className="search-books-bar">
                     <Link className="close-search" to="/" />
                     <div className="search-books-input-wrapper">
-                        {/*
-                        NOTES: The search from BooksAPI is limited to a particular set of search terms.
-                        You can find these search terms here:
-                        https://github.com/udacity/reactnd-project-myreads-starter/blob/master/SEARCH_TERMS.md
-
-                        However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
-                        you don't find a specific author or title. Every search is limited by search terms.
-                        */}
                         <input id="query" type="text" onChange={(event)=>this._search(event.target.value)} placeholder="Search by title or author"/>
-
                     </div>
                 </div>
                 <div className="search-books-results">
