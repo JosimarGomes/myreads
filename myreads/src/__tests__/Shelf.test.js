c636353c567fadbb4126cf2f3e7b24589f13aa42
JosimarGomes <josimar.gomes@superlogica.com>
2017-10-18 00:30:20 -0200

    Delete Shelf.test.js

--- myreads/src/__tests__/Shelf.test.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import {shallow} from 'enzyme';
-import Shelf from '../components/Shelf';
-
-it('renderizou a estante', () => {
-	shallow(<Shelf renderLoading={jest.fn()}/>)
-});
